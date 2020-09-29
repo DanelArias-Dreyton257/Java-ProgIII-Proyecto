d05b6884704bd66d14b7ae97afd1a910c2165e6b
danel <danel@DESKTOP-3DCJVBQ>
2020-09-29 20:39:49 +0200

    Anyadidas funcionalidades a la creacion de personajes
    Anyadido: ahora los personajes aparecen ordenados por nombre, tarda mas
    al iniciarse y al cerrarse pues en vez de escribir solo lo nuevo y
    ahora, lee todo y reescribe el fichero entero para que vaya ordenado.
    Ahora tambien, se puede seleccionar un elemento de la lista y que se
    muestre su informacion en la ventana. CUIDADO, la edicion no se guarda y
    en caso de querer editar seria mejor hacerlo manualmente desde el
    fichero porque si se vuelve a dar a OK guardaria un duplicado pero con
    las modificaciones.
    Anyadido por lo tanto un boton clear que "limpia" de datos la ventana
    Corregido que ahora los combobox tambien se "restablecen" tras pulsar el
    boton de OK, tambien con el de clear

--- ProyectoProgIII/src/soloDesarrollo/CreacionVen.java
+++ ProyectoProgIII/src/soloDesarrollo/CreacionVen.java
@@ -3,14 +3,16 @@ package soloDesarrollo;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
@@ -41,11 +43,12 @@ public class CreacionVen extends JFrame {
 	private JCheckBox ch2tipos = new JCheckBox("2 tipos");
 	private JTextArea taDescr = new JTextArea(10, 50);
 	private JButton btOk = new JButton("OK");
+	private JButton btClear = new JButton("Clear");
 	private DefaultListModel<String> mdLista = new DefaultListModel<>();
 	private JList<String> lsNombres = new JList<>();
 	private JScrollPane spnPersonajes;
 
-	private ArrayList<String> listaPer = new ArrayList<>();
+	private TreeSet<String> listaPer = new TreeSet<>();
 
 	private JLabel lbNombre = new JLabel("Nombre:");
 	private JLabel lbTipo1 = new JLabel("Tipo1:");
@@ -61,7 +64,7 @@ public class CreacionVen extends JFrame {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setSize(WIDTH, HEIGHT);
 		setResizable(false);
-		
+
 		try {
 			Scanner sc = new Scanner(new File("src/soloDesarrollo/ficheros/personajes.txt"));
 			while (sc.hasNextLine()) {
@@ -69,6 +72,7 @@ public class CreacionVen extends JFrame {
 				int i = l.indexOf("\t");
 				String p = l.substring(0, i);
 				mdLista.addElement(p);
+				listaPer.add(l);
 			}
 			sc.close();
 			lsNombres.setModel(mdLista);
@@ -92,7 +96,8 @@ public class CreacionVen extends JFrame {
 		pnCentral.add(taDescr);
 
 		pnBot.add(btOk);
-		
+		pnBot.add(btClear);
+
 		spnPersonajes = new JScrollPane(lsNombres);
 		pnIzq.add(spnPersonajes);
 
@@ -127,6 +132,35 @@ public class CreacionVen extends JFrame {
 
 		});
 
+		btClear.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clear();
+			}
+
+		});
+
+		lsNombres.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int index = lsNombres.getSelectedIndex();
+				int i = 0;
+				String l = null;
+				for (String s : listaPer) {
+					if (i == index) {
+						l = s;
+						break;
+					}
+					i++;
+				}
+				if (l != null)
+					fillWith(l);
+
+			}
+		});
+
 		addWindowListener(new WindowAdapter() {
 
 			@Override
@@ -137,7 +171,7 @@ public class CreacionVen extends JFrame {
 
 						try {
 							PrintWriter fs = new PrintWriter(
-									new FileWriter("src/soloDesarrollo/ficheros/personajes.txt", true));
+									new FileWriter("src/soloDesarrollo/ficheros/personajes.txt"));
 
 							for (String s : listaPer) {
 								fs.println(s);
@@ -161,7 +195,53 @@ public class CreacionVen extends JFrame {
 	private void clear() {
 		txNombre.setText("");
 		taDescr.setText("");
+		cbTipo1.setSelectedIndex(0);
+		cbTipo2.setSelectedIndex(0);
+		ch2tipos.setSelected(false);
+		cbTipo2.setEnabled(false);
 
+	}
+
+	private void fillWith(String l) {
+		String t = "\t";
+
+		int a = l.indexOf(t);
+		String nombre = l.substring(0, a);
+
+		int b = l.indexOf(t, a + 1);
+		String tipo1 = l.substring(a + 1, b);
+
+		int c = l.indexOf(t, b + 1);
+		String tipo2 = l.substring(b + 1, c);
+
+		String desc = l.substring(c + 1);
+
+		txNombre.setText(nombre);
+
+		Tipo t1 = Tipo.FUEGO;
+		Tipo t2 = Tipo.FUEGO;
+
+		boolean unTipo = tipo2.equals("NULL");
+
+		for (Tipo tipo : Tipo.values()) {
+			if (tipo.toString().equals(tipo1)) {
+				t1 = tipo;
+
+			}
+			if (!unTipo && tipo.toString().equals(tipo2)) {
+				t2 = tipo;
+
+			}
+		}
+
+		cbTipo1.setSelectedIndex(mdTipos.getIndexOf(t1));
+		cbTipo2.setSelectedIndex(mdTipos2.getIndexOf(t2));
+
+		ch2tipos.setSelected(!unTipo);
+		cbTipo2.setEnabled(!unTipo);
+
+		taDescr.setText(desc);
+		revalidate();
 	}
 
 }
