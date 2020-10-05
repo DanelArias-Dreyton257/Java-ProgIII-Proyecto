10ea1c2afefa819e3687d6f8efd8f458880baa6f
rdosp <rdosp@MSI>
2020-10-05 20:47:02 +0200

    Añadidos los atributos de los movimientos

--- ProyectoProgIII/src/personaje/atributos/Movimiento.java
+++ ProyectoProgIII/src/personaje/atributos/Movimiento.java
@@ -1,5 +1,43 @@
 package personaje.atributos;
 
 public class Movimiento {
+ Tipo[] tipo;
+ int Potencia;
+ int Precision;
+ 
+ /**
+	 * Devuelve la potencia del movimiento
+	 * 
+	 * @return
+	 */
+ public int getPotencia() {
+	return Potencia;
+}
+ /**
+	 * Devuelve la precision del movimiento
+	 * 
+	 * @return
+	 */
+ public int getPrecision() {
+	return Precision;
+}
+ 
+ /**
+	 * Introduce la potencia del movimiento
+	 * 
+	 * @return
+	 */
+ public void setPotencia(int potencia) {
+	Potencia = potencia;
+}
+ /**
+	 * Introduce la precision del movimiento
+	 * 
+	 * @return
+	 */
+ public void setPrecision(int precision) {
+	Precision = precision;
+}
+ 
 
 }
