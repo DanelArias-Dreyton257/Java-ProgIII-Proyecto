02110b8f0a71add5a0b8a686926e8fbaa5fa7765
danel <danel@10.164.59.78>
2020-10-06 10:08:46 +0200

    Solo he hecho Ctrl+SHIFT+F para que lo organice todo xd

--- ProyectoProgIII/src/personaje/atributos/Movimiento.java
+++ ProyectoProgIII/src/personaje/atributos/Movimiento.java
@@ -1,43 +1,44 @@
 package personaje.atributos;
 
 public class Movimiento {
- Tipo[] tipo;
- int Potencia;
- int Precision;
- 
- /**
+	Tipo[] tipo;
+	int Potencia;
+	int Precision;
+
+	/**
 	 * Devuelve la potencia del movimiento
 	 * 
 	 * @return
 	 */
- public int getPotencia() {
-	return Potencia;
-}
- /**
+	public int getPotencia() {
+		return Potencia;
+	}
+
+	/**
 	 * Devuelve la precision del movimiento
 	 * 
 	 * @return
 	 */
- public int getPrecision() {
-	return Precision;
-}
- 
- /**
+	public int getPrecision() {
+		return Precision;
+	}
+
+	/**
 	 * Introduce la potencia del movimiento
 	 * 
 	 * @return
 	 */
- public void setPotencia(int potencia) {
-	Potencia = potencia;
-}
- /**
+	public void setPotencia(int potencia) {
+		Potencia = potencia;
+	}
+
+	/**
 	 * Introduce la precision del movimiento
 	 * 
 	 * @return
 	 */
- public void setPrecision(int precision) {
-	Precision = precision;
-}
- 
+	public void setPrecision(int precision) {
+		Precision = precision;
+	}
 
 }
