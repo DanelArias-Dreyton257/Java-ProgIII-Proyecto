3530d7a36cdd9ed7d210b9020ed8c41e477b0e27
danel <danel@10.164.59.78>
2020-10-15 13:09:00 +0200

    Anyadida MAS O MENOS la fusion

--- ProyectoProgIII/src/personaje/Leyenda.java
+++ ProyectoProgIII/src/personaje/Leyenda.java
@@ -1,6 +1,11 @@
 package personaje;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 import personaje.atributos.Habilidad;
+import personaje.atributos.Tipo;
 
 /**
  * 
@@ -9,11 +14,26 @@ import personaje.atributos.Habilidad;
  */
 public class Leyenda extends Especie {
 	// Estado estado;
-	Habilidad[] movimientos = new Habilidad[4];
+	Habilidad[] habilidades = new Habilidad[4];
 	int ataque;
 	int defensa;
 	int velocidad;
 	int vida;
+
+	/**
+	 * Metodo provisional--JON ANDER DEBES REVISAR SI ESTO ES NECESARIO-- Dejarlo
+	 * para que compile pero falta hacer los constructores de verdad
+	 * 
+	 * @param esp
+	 * @param nAtk
+	 * @param nDef
+	 * @param nVel
+	 * @param nVida
+	 * @param habsElegidas
+	 */
+	public Leyenda(Especie esp, int nAtk, int nDef, int nVel, int nVida, Habilidad[] habsElegidas) {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * devuelve el ataque del personaje
@@ -52,12 +72,12 @@ public class Leyenda extends Especie {
 	}
 
 	/**
-	 * devuelve los movimientos del personaje
+	 * devuelve las habilidades del personaje
 	 * 
 	 * @return
 	 */
-	public Habilidad[] getMovimientos() {
-		return movimientos;
+	public Habilidad[] getHabilidades() {
+		return habilidades;
 	}
 
 	/**
@@ -120,7 +140,7 @@ public class Leyenda extends Especie {
 	 * @param movimientos
 	 */
 	public void setMovimientos(Habilidad[] movimientos) {
-		this.movimientos = movimientos;
+		this.habilidades = movimientos;
 	}
 
 	/**
@@ -131,7 +151,67 @@ public class Leyenda extends Especie {
 	 * @return personaje fusionado
 	 */
 	public static Leyenda fusion(Leyenda p1, Leyenda p2) {
-		// TODO
+		// Calculo de los atributos numericos
+		int extraPts = 25;
+		int nAtk = ((p1.getAtaque() + p2.getAtaque()) / 2) + extraPts;
+		int nDef = ((p1.getDefensa() + p2.getDefensa()) / 2) + extraPts;
+		int nVel = ((p1.getVelocidad() + p2.getVelocidad()) / 2) + extraPts;
+		int nVida = ((p1.getVida() + p2.getVida()) / 2) + extraPts;
+
+		// Seleccion de los movimientos
+		ArrayList<Habilidad> posHabs = new ArrayList<>();
+		posHabs.addAll(Arrays.asList(p1.getHabilidades()));
+		posHabs.addAll(Arrays.asList(p2.getHabilidades()));
+		int numHabs = 4;
+		Habilidad[] habsElegidas = new Habilidad[numHabs];
+		for (int x = 0; x < numHabs; x++) {
+			Random r = new Random();
+			int id = r.nextInt(posHabs.size());
+			habsElegidas[x] = posHabs.get(id);
+			posHabs.remove(id);
+		}
+		Especie esp = null;
+		while (esp == null) {
+			// Seleccion de los tipos
+			ArrayList<Tipo> posTipos = new ArrayList<>();
+			posTipos.addAll(Arrays.asList(p1.getTipos()));
+			posTipos.addAll(Arrays.asList(p2.getTipos()));
+			Tipo tipo1 = null;
+			int id = 0;
+			while (tipo1 == null) {
+				Random r = new Random();
+				id = r.nextInt(posTipos.size());
+				tipo1 = posTipos.get(id);
+			}
+
+			Tipo tipo2 = tipo1;
+			while (tipo2.equals(tipo1)) {
+				posTipos.remove(id); // primero elimina el tipo elegido en el primer while y luego va borrando el
+										// anterior seleccionado ya que ha vuelto a ejecutar el while por lo que
+										// significa que es un tipo no valido
+				Random r = new Random();
+				id = r.nextInt(posTipos.size());
+				tipo2 = posTipos.get(id);
+			}
+
+			esp = buscarEspecieEnBD(tipo1, tipo2);
+		}
+
+		Leyenda l = new Leyenda(esp, nAtk, nDef, nVel, nVida, habsElegidas);
+
+		return l;
+	}
+
+	/**
+	 * --METODO PROVISIONAL--Dejarlo para que compile pero falta hacer los
+	 * constructores de verdad
+	 * 
+	 * @param tipo1
+	 * @param tipo2
+	 * @return
+	 */
+	private static Especie buscarEspecieEnBD(Tipo tipo1, Tipo tipo2) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
