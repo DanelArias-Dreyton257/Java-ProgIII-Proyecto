889bb82e22b40b934c782926bce78475cadaf745
rdosp <rdosp@MSI>
2020-11-03 20:49:47 +0100

    Añadido los text de habilidad, y de jugador

---
+++ ProyectoProgIII/test/objetos/JugadorTest.java
@@ -0,0 +1,50 @@
+package objetos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import personaje.Leyenda;
+import personaje.atributos.Habilidad;
+import personaje.atributos.Tipo;
+
+public class JugadorTest {
+	Jugador j1= new Jugador("J1");
+	Tipo[] tipitos= {Tipo.FUEGO,Tipo.AGUA};
+	 Habilidad[] nada= new Habilidad[4];
+	 Leyenda l1= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 12, 32, 32, 123);
+	 Leyenda l2= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 43, 32, 45, 123);
+	 Leyenda l3= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 13, 32, 56, 123);
+	 Leyenda l4= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 22, 32, 67, 123);
+	 Leyenda l5= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 33, 32, 34, 123);
+	 Leyenda l6= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 44, 32, 22, 123);
+	 int Numero=6;
+	 public void setUp() {
+			
+	
+	 }
+	 @Test
+	public void test() {
+		j1.anyadirAEquipo(0, l1);
+		j1.anyadirAEquipo(1, l5); 
+		j1.anyadirNuevasLeyendas(l4,l6,l2,l3);
+		j1.anyadirNuevasLeyendas(l4,l6,l2,l3);
+		assertEquals(Numero, j1.getNumLeyendasEnEquipo());
+		assertEquals(l6, j1.getLeyendaEquipo(3));
+		assertEquals(4, j1.getNumLeyendasEnEternidad());
+		assertEquals(6, j1.getNumLeyendasEnEquipo());
+		assertEquals(l3, j1.getLeyendaEternidad(3));
+	}
+	 @Test(expected=ArrayIndexOutOfBoundsException.class)
+		public void testErrores() {
+		 	j1.anyadirAEquipo(0, l1);
+			j1.anyadirAEquipo(1, l5); 
+			j1.anyadirNuevasLeyendas(l4,l6,l2,l3);
+			j1.anyadirNuevasLeyendas(l4,l6,l2,l3);
+			assertEquals(Numero, j1.getNumLeyendasEnEquipo());
+			assertEquals(l1, j1.getLeyendaEquipo(7));
+			assertEquals(3, j1.getNumLeyendasEnEternidad());
+			assertEquals(1, j1.getNumLeyendasEnEquipo());
+			assertEquals(l3, j1.getLeyendaEternidad(2));
+		}
+}
---
+++ ProyectoProgIII/test/personaje/atributos/HabilidadTest.java
@@ -0,0 +1,51 @@
+package personaje.atributos;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import personaje.atributos.Habilidad;
+
+public class HabilidadTest {
+public Habilidad h1;
+
+	@Before
+	public void setUp() {
+	h1= new Habilidad("Rayo", Tipo.ELECTRICIDAD, 53, 0.2);
+
+
+	}
+
+	@After
+	public void tearDown() {
+
+	}
+
+	@Test
+	public void testSetPotencia() {
+	h1.setPotencia(34);
+	assertEquals(34, h1.getPotencia());
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetPotencia1() {
+	h1.setPotencia(-32);
+	h1.setPotencia(31232);
+	}
+
+	@Test
+	public void testSetPrecision() {
+	h1.setPrecision(0.4);
+	
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetPrecision1() {
+	h1.setPrecision(-0.4);
+	h1.setPrecision(31232);
+	h1.setPrecision(2);
+	}
+
+
+
+}
