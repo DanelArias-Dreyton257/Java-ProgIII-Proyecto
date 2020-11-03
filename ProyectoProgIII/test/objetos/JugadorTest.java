92b87eb2b19f1d2d6e52a8b453e03102962c44d2
rdosp <rdosp@MSI>
2020-11-03 23:18:13 +0100

    Añadida mas cosas en el text de jugador

--- ProyectoProgIII/test/objetos/JugadorTest.java
+++ ProyectoProgIII/test/objetos/JugadorTest.java
@@ -13,11 +13,11 @@ public class JugadorTest {
 	Tipo[] tipitos= {Tipo.FUEGO,Tipo.AGUA};
 	 Habilidad[] nada= new Habilidad[4];
 	 Leyenda l1= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 12, 32, 32, 123);
-	 Leyenda l2= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 43, 32, 45, 123);
-	 Leyenda l3= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 13, 32, 56, 123);
-	 Leyenda l4= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 22, 32, 67, 123);
-	 Leyenda l5= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 33, 32, 34, 123);
-	 Leyenda l6= new Leyenda("Zeus", "Dios del rayo", tipitos, nada, 44, 32, 22, 123);
+	 Leyenda l2= new Leyenda("Poseidon", "Dios del mar", tipitos, nada, 43, 32, 45, 123);
+	 Leyenda l3= new Leyenda("Afordita", "Diosa del amor", tipitos, nada, 13, 32, 56, 123);
+	 Leyenda l4= new Leyenda("Hades", "Dios del inframundo", tipitos, nada, 22, 32, 67, 123);
+	 Leyenda l5= new Leyenda("Hercules", "Dios del poder", tipitos, nada, 33, 32, 34, 123);
+	 Leyenda l6= new Leyenda("Shiva", "Dios de la destruccion", tipitos, nada, 44, 32, 22, 123);
 	 int Numero=6;
 	 public void setUp() {
 			
@@ -33,8 +33,12 @@ public class JugadorTest {
 		assertEquals(l6, j1.getLeyendaEquipo(3));
 		assertEquals(4, j1.getNumLeyendasEnEternidad());
 		assertEquals(6, j1.getNumLeyendasEnEquipo());
-		assertEquals(l3, j1.getLeyendaEternidad(3));
+		assertEquals(l2, j1.getLeyendaEternidad(2));
+		j1.getLeyendaEternidad(2);
+		j1.delLeyendaEternidad(2);
+		assertEquals(l3, j1.getLeyendaEternidad(2));
 	}
+	 
 	 @Test(expected=ArrayIndexOutOfBoundsException.class)
 		public void testErrores() {
 		 	j1.anyadirAEquipo(0, l1);
@@ -46,5 +50,6 @@ public class JugadorTest {
 			assertEquals(3, j1.getNumLeyendasEnEternidad());
 			assertEquals(1, j1.getNumLeyendasEnEquipo());
 			assertEquals(l3, j1.getLeyendaEternidad(2));
+			j1.delLeyendaEternidad(23);;
 		}
 }
