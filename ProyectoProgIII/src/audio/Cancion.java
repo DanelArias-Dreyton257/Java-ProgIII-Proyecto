1b4680c595965b88f1ec047d720e4cdbd2689fad
danel <danel@DESKTOP-3DCJVBQ>
2021-01-18 18:29:37 +0100

    Anyadida clase Cancion POR FINN

--- ProyectoProgIII/src/audio/Cancion.java
+++ ProyectoProgIII/src/audio/Cancion.java
@@ -9,6 +9,11 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * 
+ * @author danel y jon ander
+ *
+ */
 public class Cancion {
 
 	public static final int FINALIZADO = 0;
@@ -21,21 +26,43 @@ public class Cancion {
 	private Clip audioClip;
 	private int estado = FINALIZADO;
 
+	/**
+	 * Crea una cancion a partir del fichero de audio ".wav"
+	 * 
+	 * @param audioFile
+	 * @throws SongException
+	 * @throws NoExistingFileException
+	 */
 	public Cancion(File audioFile) throws SongException, NoExistingFileException {
 
 		if (audioFile.exists()) {
 			this.ficheroAudio = audioFile;
-			resetAudioStream();
 
+			try {
+				audioInputStream = AudioSystem.getAudioInputStream(ficheroAudio.getAbsoluteFile());
+				audioClip = AudioSystem.getClip();
+				audioClip.open(audioInputStream);
+				audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+				e.printStackTrace();
+				throw new SongException("Error abriendo el archivo", e);
+			}
 		} else
 			throw new NoExistingFileException("El fichero de audio debe existir");
 	}
 
+	/**
+	 * Inicia la reproduccion de la cancion
+	 */
 	public void reproducir() {
 		audioClip.start();
 		estado = REPRODUCIENDO;
 	}
 
+	/**
+	 * Pausa la reproducion de la cancion solo si estaba reproduciendose, NO la
+	 * finaliza
+	 */
 	public void pausar() {
 		if (estado == REPRODUCIENDO) {
 			this.frameActual = this.audioClip.getMicrosecondPosition();
@@ -44,6 +71,11 @@ public class Cancion {
 		}
 	}
 
+	/**
+	 * Reanuda la cancion si estaba pausada
+	 * 
+	 * @throws SongException
+	 */
 	public void reanudar() throws SongException {
 		if (estado == PAUSADO) {
 			audioClip.close();
@@ -53,6 +85,11 @@ public class Cancion {
 		}
 	}
 
+	/**
+	 * Reinicia la cancion
+	 * 
+	 * @throws SongException
+	 */
 	public void reiniciar() throws SongException {
 		audioClip.stop();
 		audioClip.close();
@@ -60,15 +97,26 @@ public class Cancion {
 		frameActual = 0L;
 		audioClip.setMicrosecondPosition(0);
 		this.reproducir();
-		;
+
 	}
 
+	/**
+	 * Finaliza la reproduccion de la cancion
+	 */
 	public void finalizar() {
 		frameActual = 0L;
 		audioClip.stop();
 		audioClip.close();
+		estado = FINALIZADO;
 	}
 
+	/**
+	 * Salta al milisegundo indicado si el punto a saltar de la cancion es positivo
+	 * y menor que la duracion de la cancion
+	 * 
+	 * @param millis
+	 * @throws SongException
+	 */
 	public void saltar(long millis) throws SongException {
 		long micros = millis * 1000;
 		if (micros > 0 && micros < audioClip.getMicrosecondLength()) {
@@ -78,36 +126,70 @@ public class Cancion {
 			frameActual = micros;
 			audioClip.setMicrosecondPosition(micros);
 			this.reproducir();
-		}
+		} else
+			throw new SongException(
+					"El punto a saltar de la cancion debe ser positivo y menor que la duracion de la cancion");
 	}
-	
+
+	/**
+	 * Devuelve la duracion de la cancion en milisegundos
+	 * 
+	 * @return
+	 */
 	public long getDuracionEnMillis() {
 		return Math.floorDiv(audioClip.getMicrosecondLength(), 1000);
 	}
-	
+
+	/**
+	 * Devuelve el frame actual de la cancion en milisegundos
+	 * 
+	 * @return
+	 */
 	public long getFrameActualEnMillis() {
 		return Math.floorDiv(audioClip.getMicrosecondPosition(), 1000);
 	}
-	
+
+	/**
+	 * Devuelve la duracion de la cancion en microsegundos
+	 * 
+	 * @return
+	 */
 	public long getDuracionEnMicros() {
 		return audioClip.getMicrosecondLength();
 	}
-	
+
+	/**
+	 * Devuelve el frame actual de la cancion en microsegundos
+	 * 
+	 * @return
+	 */
 	public long getFrameActualEnMicros() {
 		return frameActual;
 	}
 
-	public void resetAudioStream() throws SongException {
+	/**
+	 * Resetea el Audio Stream releyendo el fichero
+	 * 
+	 * @throws SongException
+	 */
+	private void resetAudioStream() throws SongException {
 		try {
 			audioInputStream = AudioSystem.getAudioInputStream(ficheroAudio.getAbsoluteFile());
-			audioClip = AudioSystem.getClip();
 			audioClip.open(audioInputStream);
+			audioClip.loop(Clip.LOOP_CONTINUOUSLY);
 		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
 			e.printStackTrace();
 			throw new SongException("Error reseteando el AudioInputStream", e);
 		}
 	}
+	
+	
 
+	/**
+	 * 
+	 * @author danel
+	 *
+	 */
 	public class NoExistingFileException extends Exception {
 
 		private static final long serialVersionUID = 1L;
@@ -122,6 +204,11 @@ public class Cancion {
 
 	}
 
+	/**
+	 * 
+	 * @author danel
+	 *
+	 */
 	public class SongException extends Exception {
 
 		private static final long serialVersionUID = 1L;
@@ -132,20 +219,6 @@ public class Cancion {
 
 		public SongException(String msg, Throwable t) {
 			super(msg, t);
-		}
-
-	}
-
-	public static void main(String[] args) {
-
-		Cancion c;
-		try {
-			c = new Cancion(new File("C:/Users/danel/Downloads/Violet Sky.wav"));
-			c.reproducir();
-			Thread.sleep(100000);
-		} catch (SongException | NoExistingFileException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 
 	}
